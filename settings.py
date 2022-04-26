99ddd8f621b3a5a2ff679e05533b658fa4c8fe4d
Joel Onojason <joel.onojason@outlook.com>
2022-04-26 18:15:10 +0100

    folder and settings setup for the project

---
+++ settings.py
@@ -0,0 +1,14 @@
+import os
+
+# for news api
+NEWSAPI_API_KEY = os.getenv('NEWSAPI_API_KEY', 'f86b6890a1354ca3b491ae68b3a850d5')
+
+# for reddit, since they is no direct API for news, mimic a browser agent to make this call
+REQUEST_HEADERS = {
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 '
+                  'Safari/537.36'
+}
+
+# To avoid the application waiting for the request forever since request are blocking in nature until there's a response
+# from the destination server
+REQUEST_TIMEOUT = 10  # secs
